48da0ef680e57241cae37c6bd79e6e306fed2ab1
BaiDom <dombaily88@gmail.com>
2024-11-12 16:49:56 +0000

    updated api key

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 import { useEffect, useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (lat !== null && long !== null) {
+        try {
+          const response = await fetch(
+            `${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
+          );
+          const result = await response.json();
+          setData(result);
+          console.log(result);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    };
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    fetchData();
+  }, [lat, long]);
+
+  return (
+    <div className="App">
+      lat is:{lat} long is: {long}
+    </div>
+  );
 }
 
 export default App;
